ab31ab22117b9a838961bd06553c6795307423ef
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2022-04-02 10:51:40 +0100

    init project from root-learn

---
+++ 2.1-enum.ts
@@ -0,0 +1,28 @@
+// Enum-Tuple-Intrfaces challenge
+
+// ######################################
+//   PART 1 - ENUM
+// ######################################
+
+/* 
+-------------------------------------------------------
+  3.1 Update the type 'CompassDirection' to be 
+    - North, East, South or West
+-------------------------------------------------------
+*/
+
+enum CompassDirection {
+    East,
+    North,
+ }
+ 
+ const startingDirection = CompassDirection.East;
+ const endingDirection = CompassDirection.North;
+ 
+ console.log(CompassDirection[2]) // guess the output and think why ?
+ 
+ /* 
+ -------------------------------------------------------
+   3.1 END
+ -------------------------------------------------------
+ */
